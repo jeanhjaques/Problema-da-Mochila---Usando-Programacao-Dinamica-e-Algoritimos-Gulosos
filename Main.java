a7886121f30a6c435b6347ada913ddf4de38376b
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2021-10-27 18:35:56 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,109 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main{
-	public static void main(String[] args){
-		int [] valor = {100, 30, 20, 50, 20, 20, 50};
-		int [] peso = {15, 10, 10, 10, 30, 10, 10};
+	public static void main(String[] args) throws IOException{
 
-		for(int i = 0; i<valor.length; i++){
-			double precoPorKg = (double)valor[i]/(double)peso[i];
-			System.out.println(i+"- "+valor[i]+" "+peso[i]+" = "+precoPorKg);
+		File arquivo = new File( "./resultado.csv");
+
+		arquivo.createNewFile();
+
+		if(arquivo.exists()){
+			arquivo.delete();
 		}
 
-		double[] mochila = ProblemaMochila.fractionalKnapsack(valor, peso, 49);
+		FileWriter fw = new	FileWriter(arquivo, true);
+		BufferedWriter bw = new	BufferedWriter(fw);
 
-		System.out.println (Arrays.toString(mochila));
+		//prepara cabeçalho para a planilha
+		String cabecalho = "W, n, bin, tf, tb";
 
+		bw.write(cabecalho);
+
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Entre com o tamanho máximo da mochila: ");
+		int max = ler.nextInt();
+
+
+
+		System.out.println("   W       |    n   |     bin    |      tf     |     tb    |");
+		for(int W=10;W<=max; W = W + 5){
+
+			//determina o tamanho do vetor que armazena os n
+			int tamanhoVetorN = 0;
+			for(int i=W/2;i<=W*2; i= i+5){
+				tamanhoVetorN++;
+			}
+			
+			//constrói vetor de n
+			int[] n = new int[tamanhoVetorN];
+			int iterador = 0;
+			for(int i=W/2;i<=W*2; i= i+5){
+				n[iterador] = i;
+				iterador++;
+			}
+
+
+			for(int i=0; i<n.length; i++){
+				int[] v = new int[n[i]]; //vetor de valores
+				int[] w = new int[n[i]]; //vetor de pesos
+
+				//preenche vetor de valores(v) e de pesos(w)
+				Random random = new Random();
+				for(int j=0; j<v.length; j++){
+					v[j] = random.nextInt(200);
+					w[j] = random.nextInt(W-1)+1;
+				}
+
+				//Testa usando mochila binaria
+	            long tempoInicial = System.nanoTime();
+	            double[][] mochilaBinaria = ProblemaMochila.bynaryKnapsack(v, w, W);
+	            long tempoFinal = System.nanoTime();
+
+	            long tempoMochilaBinaria = tempoFinal - tempoInicial;
+	            tempoInicial = 0;
+	            tempoFinal = 0;
+
+	            String tempoMochilaBinariaFormatado = String.format("%.6f",(double)tempoMochilaBinaria/1000000000);
+	            double valorMochilaBinaria = mochilaBinaria[v.length-1][W-1];
+
+	            //Testa usando mochila fracionaria
+	           	tempoInicial = System.nanoTime();
+	            double[] mochilaFracionaria = ProblemaMochila.fractionalKnapsack(v, w, W);
+	            tempoFinal = System.nanoTime();
+
+	            long tempoMochilaFracionaria = tempoFinal - tempoInicial;
+	            tempoInicial = 0;
+	            tempoFinal = 0;
+
+	           	String tempoMochilaFracionariaFormatado = String.format("%.6f",(double)tempoMochilaFracionaria/1000000000);
+
+
+	            //imprime resultados
+				System.out.println("  "+W+"            "+n[i]+"        "+valorMochilaBinaria+"     "+tempoMochilaFracionariaFormatado+"      "+tempoMochilaBinariaFormatado);
+
+				String bin = String.valueOf(valorMochilaBinaria).replace("." , ",");
+
+				bw.newLine();
+				bw.write(W+","+n[i]+",\""+bin+"\",\""+tempoMochilaFracionariaFormatado+"\",\""+tempoMochilaBinariaFormatado+"\"");
+
+				
+			}
+
+
+		}
+
+		bw.close();
+		fw.close();
+		
+		System.out.println("Um arquivo resultado.csv com o resultado desta execução agora pode ser encontrado na raiz do programa.");
 	}
 }
--- ProblemaMochila.java
+++ ProblemaMochila.java
@@ -1,10 +1,11 @@
+import java.lang.Math;
+
 public class ProblemaMochila{
-	public static double[] fractionalKnapsack(int[] valor, int[] peso, int pesoMaximo){
+	public static double[] fractionalKnapsack(int[] valor, int[] peso, double pesoMaximo){
 		//ordenando os vetores valor e peso pelo seu preço por quilo
 		//optamos pelo bubbleSort
 		for(int i = 0; i<valor.length; i++){
 			for(int j = 0; j<valor.length - 1; j++){
-				System.out.println(j);
 				if(((double)valor[j]/(double)peso[j]) > ((double)valor[j+1]/(double)peso[j+1])){
 					int aux = valor[j];
                     valor[j] = valor[j + 1];
@@ -16,7 +17,7 @@ public class ProblemaMochila{
 			}
 		}
 
-		int i = 0;
+		int i = 1;
 		double[] mochila = new double[10];
 
 		while (peso[i] <= pesoMaximo){
@@ -31,4 +32,44 @@ public class ProblemaMochila{
 
 		return mochila;
 	}
-}
+
+	public static double[][] bynaryKnapsack(int[] valor, int[] peso, int pesoMaximo){
+		int n = valor.length;
+		double[][] mochila = new double[n+1][pesoMaximo+1];
+
+		for(int i=0; i<n; i++){
+			mochila[i][0] = 0;
+		}
+		for(int j=1;j<pesoMaximo;j++){
+			mochila[0][j] = 0;
+		}
+
+		for(int i=1; i<n; i++){
+			for(int j=1; j<pesoMaximo;j++){
+				if(j<peso[i]){
+					mochila[i][j] = mochila[i-1][j];
+				}
+				else{
+					mochila[i][j] = Math.max(mochila[i-1][j], mochila[i-1][j-peso[i]]+valor[i]);
+				}
+			}
+		}
+
+		/*
+		for(double[] row : mochila) {
+            printRow(row);
+        }
+        */
+
+		return mochila;
+	}
+
+	//auxilia na impressão da matriz da saída do algoritmo  da mochila binária
+	public static void printRow(double[] row) {
+        for (double i : row) {
+            System.out.print(i);
+            System.out.print("\t");
+        }
+        System.out.println();
+    }
+}  
